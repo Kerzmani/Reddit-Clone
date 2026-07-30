b83041215dce0bd0483a0878721f25f0e391eed2
Kerzmani <KerzmanDeveloping@gmail.com>
2026-07-30 14:51:54 -0500

    feat: Added rendering for existing and nonexisting subreddits on Subreddit page

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,13 +1,19 @@
-import {defineSchema, defineTable} from "convex/server";
-import {v} from "convex/values";
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
 
 export const schema = defineSchema({
   users: defineTable({
     username: v.string(),
-    externalId: v.string()
+    externalId: v.string(),
   })
     .index("byExternalId", ["externalId"])
-    .index("byUsername", ["username"])
+    .index("byUsername", ["username"]),
+
+  subreddit: defineTable({
+    name: v.string(),
+    description: v.optional(v.string()),
+    authorId: v.id("users"),
+  })
 });
 
 export default schema;
